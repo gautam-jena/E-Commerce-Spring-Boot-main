b369d4ce6725506f78e362bce34a81961f0cb86a
admin <gautamjena383@gmail.com>
2024-04-15 23:44:04 +0530

    spring boot application

--- product-service/src/main/java/com/selimhorri/app/ProductServiceApplication.java
+++ product-service/src/main/java/com/selimhorri/app/ProductServiceApplication.java
@@ -10,6 +10,8 @@ public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
+
+		// is ready for prod
 	}
 	
 	
